b00e471ffa1e22f1fa1df217a9e7d169e24973a5
yongjie1996 <yongjie.lim.1996@gmail.com>
2023-08-18 10:57:08 +0800

    Manga files added with more functionality

--- my-mangastore/src/main/java/Manga.java
+++ my-mangastore/src/main/java/Manga.java
@@ -13,5 +13,45 @@ public class Manga {
     private String title;
     private String author;
     private String genre;
-    // Add other properties and getters/setters
+
+    public Manga() {
+    }
+
+    public Manga(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
--- my-mangastore/src/main/java/MangaRepository.java
+++ my-mangastore/src/main/java/MangaRepository.java
@@ -1,5 +1,22 @@
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MangaRepository extends JpaRepository<Manga, Long> {
-    // Add custom query methods if needed
+    // Find books by title
+    List<Manga> findByTitleContainingIgnoreCase(String title);
+
+    // Find books by author
+    List<Manga> findByAuthorContainingIgnoreCase(String author);
+
+    // Find books by category
+    List<Manga> findByGenre(String Genre);
+
+    // Find books by title and author
+    List<Manga> findByTitleContainingIgnoreCaseAndAuthorContainingIgnoreCase(String title, String author);
+
+    // Find books by title or author
+    List<Manga> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String title, String author);
+
+    // Additional custom query methods can be added here
 }
