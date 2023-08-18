e3deb65bcddd9d6284a0de47d833a20b8a1eca51
yongjie1996 <yongjie.lim.1996@gmail.com>
2023-08-18 14:42:13 +0800

    Fixed a mistake on the EntityScan

--- my-mangastore/src/main/java/com/mangastore/mymangastore/MyMangastoreApplication.java
+++ my-mangastore/src/main/java/com/mangastore/mymangastore/MyMangastoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "manga")
+@EntityScan(basePackages = "com.mangastore.mymangastore.model")
 public class MyMangastoreApplication {
 
 	public static void main(String[] args) {
