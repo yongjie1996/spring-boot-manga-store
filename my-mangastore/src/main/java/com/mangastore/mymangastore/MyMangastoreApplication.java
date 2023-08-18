13b587fccfb786e4c91e07fc0f954cd95a360783
yongjie1996 <yongjie.lim.1996@gmail.com>
2023-08-18 15:03:15 +0800

    bleh

--- my-mangastore/src/main/java/com/mangastore/mymangastore/MyMangastoreApplication.java
+++ my-mangastore/src/main/java/com/mangastore/mymangastore/MyMangastoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.mangastore.mymangastore.model")
+@EntityScan("com.mangastore.mymangastore.model")
 public class MyMangastoreApplication {
 
 	public static void main(String[] args) {
