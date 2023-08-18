98c3fad1d64dd8cd77b120896aadce14950c7895
yongjie1996 <yongjie.lim.1996@gmail.com>
2023-08-18 08:42:59 +0800

    Initial Commit

---
+++ my-mangastore/src/test/java/com/mangastore/mymangastore/MyMangastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangastore.mymangastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMangastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
